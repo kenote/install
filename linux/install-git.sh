6ad4c096c0df8653c8b008ded9a8dac76eefe6ce
thondery <thondery@163.com>
2022-11-05 22:58:37 +0800

    update bash of [htop,git].

--- linux/install-git.sh
+++ linux/install-git.sh
@@ -2,33 +2,46 @@
 
 git_release="git-2.37.1"
 
-# 判断是否海外网络
-function is_oversea(){
-    curl --connect-timeout 5 https://www.google.com -s --head | head -n 1 | grep "HTTP/1.[01] [23].." &> /dev/null;
+get_release_version() {
+    list=(`wget -qO- https://mirrors.edge.kernel.org/pub/software/scm/git/ | grep -E "git-[1-9]" | sed 's/<[^>]*>//g' | awk -F ' ' '{print $1}' | grep ".tar.gz"`)
+    release_version="1.0.0"
+    value=0
+    for item in ${list[@]};
+    do
+        tmpval=`echo $item | sed -E 's/(git-|\.tar\.gz)//g'`
+        if ( test "$(echo "$release_version $tmpval" | tr " " "\n" | sort -V | head -n 1)" != "$tmpval" ); then
+            release_version=$tmpval
+        fi
+    done
+    git_release="git-$release_version"
 }
 
-# 初始化环境
-function init_env(){
-    if (is_oversea); then
-        urlroot="https://raw.githubusercontent.com/kenote/install"
-    else
-        urlroot="https://gitee.com/kenote/install/raw"
-    fi
-    if [ -f './start.sh' ]; then
-        system=`./start.sh release`
-    else
-        system=`wget -O start.sh ${urlroot}/main/linux/start.sh && chmod +x start.sh && clear && ./start.sh release`
+check_sys(){
+    if [[ -f /etc/redhat-release ]]; then
+        release="centos"
+    elif cat /etc/issue | grep -q -E -i "debian"; then
+        release="debian"
+    elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+        release="ubuntu"
+    elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+        release="centos"
+    elif cat /proc/version | grep -q -E -i "debian"; then
+        release="debian"
+    elif cat /proc/version | grep -q -E -i "ubuntu"; then
+        release="ubuntu"
+    elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+        release="centos"
     fi
 }
 
 # 安装 git
-function install_git(){
+install_git() {
     echo -e "开始安装 ${git_release} ..."
     cd $HOME
-    if [[ $system == 'debian' ]]; then
+    if [[ $release == 'debian' ]]; then
         sudo apt install -y  build-essential
         sudo apt install  -y libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev
-    elif [[ $system == 'centos' ]]; then
+    elif [[ $release == 'centos' ]]; then
         sudo yum install -y curl-devel expat-devel gettext-devel openssl-devel zlib-devel gcc-c++ perl-ExtUtils-MakeMaker
         sudo yum -y remove git
     else
@@ -44,5 +57,6 @@ function install_git(){
     git --version
 }
 
-init_env
+get_release_version
+check_sys
 install_git
--- linux/install-htop.sh
+++ linux/install-htop.sh
@@ -1,32 +1,31 @@
 #! /bin/bash
 
-# 判断是否海外网络
-function is_oversea(){
-    curl --connect-timeout 5 https://www.google.com -s --head | head -n 1 | grep "HTTP/1.[01] [23].." &> /dev/null;
-}
-
-# 初始化环境
-function init_env(){
-    if (is_oversea); then
-        urlroot="https://raw.githubusercontent.com/kenote/install"
-    else
-        urlroot="https://gitee.com/kenote/install/raw"
-    fi
-    if [ -f './start.sh' ]; then
-        system=`./start.sh release`
-    else
-        system=`wget -O start.sh ${urlroot}/main/linux/start.sh && chmod +x start.sh && clear && ./start.sh release`
+check_sys(){
+    if [[ -f /etc/redhat-release ]]; then
+        release="centos"
+    elif cat /etc/issue | grep -q -E -i "debian"; then
+        release="debian"
+    elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+        release="ubuntu"
+    elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+        release="centos"
+    elif cat /proc/version | grep -q -E -i "debian"; then
+        release="debian"
+    elif cat /proc/version | grep -q -E -i "ubuntu"; then
+        release="ubuntu"
+    elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+        release="centos"
     fi
 }
 
 # 安装 htop
-function install_htop(){
+install_htop() {
     if (command -v htop); then
         echo -e "htop 已经存在, 无需安装"
     else
         echo -e "开始安装 htop ..."
         cd $HOME
-        if [[ $system == 'centos' ]]; then
+        if [[ $release == 'centos' ]]; then
             version=`cat /etc/os-release | grep "VERSION_ID" | sed 's/\(.*\)=\"\(.*\)\"/\2/g'`
             yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-$version.noarch.rpm -y
             yum install -y htop
@@ -36,5 +35,5 @@ function install_htop(){
     fi
 }
 
-init_env
+check_sys
 install_htop
